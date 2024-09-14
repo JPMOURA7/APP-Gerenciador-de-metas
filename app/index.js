0ac086fd1281510884a5dcaed329e2c797b48550
JPMoura <jp7401301@gmail.com>
2024-09-14 03:56:13 +0000

    aprendendo sobre Arrow function,Array,Var,Const,Function e concatenação

--- app/index.js
+++ app/index.js
@@ -1,3 +1,16 @@
 // o primeiro Hello world!!
+//let mensagem="ola mundo"
+//console.log(mensagem);
 
-console.log("Ola, mundo");
+let metas1 = ["trabalhar ","curso ","escola "]
+let metas2 = ["comer ","beber ","dormir "]
+console.log(metas1[2] + metas2[2])
+let meta = {
+    value:"Ir a academia",
+    checked:false,
+    log: (info) => {
+        console.log(info)
+    }
+}
+meta.value = "Parar de tomar esteróides";
+meta.log(meta.value)
