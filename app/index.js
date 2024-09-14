01cd744e52829259ef43995ab72c148ec9c4db92
JPMoura <jp7401301@gmail.com>
2024-09-14 04:27:10 +0000

    iniciando o menu

--- app/index.js
+++ app/index.js
@@ -1,21 +1,9 @@
-// o primeiro Hello world!!
-//let mensagem="ola mundo"
-//console.log(mensagem);
-
-//let metas1 = ["trabalhar ","curso ","escola "]
-//let metas2 = ["comer ","beber ","dormir "]
-console.log(metas1[2] + metas2[2])
-let meta = {
-    value:"Ir a academia",
-    checked:true,
-}
-let metas = {
-    meta,
-    {
-    value:"se alimentar de forma saúdavel",
-    checked:false,
+const start = {} => {
+    let count = 0
+    while(count < 10){
+        console.log(count)
+        count = count + 1
     }
-}
-meta.value = "Parar de tomar esteróides";
-meta.log(meta.value)
-meta.log(metas.value)
+} 
+
+start{}
