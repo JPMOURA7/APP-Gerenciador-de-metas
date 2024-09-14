5556a210105c6f42bcbb44d6faab8353f021a008
JPMoura <jp7401301@gmail.com>
2024-09-14 04:09:53 +0000

    definição de meta e metas

--- app/index.js
+++ app/index.js
@@ -2,15 +2,20 @@
 //let mensagem="ola mundo"
 //console.log(mensagem);
 
-let metas1 = ["trabalhar ","curso ","escola "]
-let metas2 = ["comer ","beber ","dormir "]
+//let metas1 = ["trabalhar ","curso ","escola "]
+//let metas2 = ["comer ","beber ","dormir "]
 console.log(metas1[2] + metas2[2])
 let meta = {
     value:"Ir a academia",
+    checked:true,
+}
+let metas = {
+    meta,
+    {
+    value:"se alimentar de forma saúdavel",
     checked:false,
-    log: (info) => {
-        console.log(info)
     }
 }
 meta.value = "Parar de tomar esteróides";
 meta.log(meta.value)
+meta.log(metas.value)
